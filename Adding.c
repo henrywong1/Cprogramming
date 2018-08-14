8b5be933e39e9a7a9be613571ded16ac5e10a633
Henrywong1 <h_wong14@u.pacific.edu>
2018-08-14 15:59:50 -0700

    Adding floats and int in C

---
+++ Adding.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a,b;
+    float c,d;
+    scanf("%d%d\n", &a, &b);
+    scanf("%f%f\n", &c, &d);
+    printf("%d %d\n", (a+b), (a-b));
+    printf("%.1f %.1f\n", (c+d), (c-d));
+
+    // Can use .1 to represent decimal places, and leaving spaces in printf will allow a space to form.
+    return 0;
+}
